f6440854deeae714d4d04764f07d016c60b57562
Mary Strodl <mstrodl@csh.rit.edu>
2024-11-10 19:27:08 -0500

    Yay xbox 360 controller working

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,21 @@ use bsp::entry;
 use core::cell::RefCell;
 use critical_section::Mutex;
 use defmt::*;
-use defmt_serial as _;
+// use defmt_serial as _;
+use defmt_rtt as _;
 use panic_probe as _;
 use static_cell::StaticCell;
 use usb_device::{class_prelude::*, prelude::*};
 
 // Provide an alias for our BSP so we can switch targets quickly.
 // Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
-use adafruit_kb2040::{self as bsp, hal::gpio::SioInput};
+use rp_pico::{self as bsp, hal::gpio::SioInput};
 // use sparkfun_pro_micro_rp2040 as bsp;
 
 use bsp::hal::{
     self,
-    clocks::{init_clocks_and_plls, Clock},
-    pac::{self, interrupt},
+    clocks::init_clocks_and_plls,
+    pac::{self},
     // prelude::*,
     watchdog::Watchdog,
     Sio,
@@ -29,109 +30,61 @@ use bsp::hal::{
 use embedded_hal::digital::InputPin;
 
 // use usb_device::{class_prelude::*, prelude::*};
+use usbd_hid::hid_class::HidClassSettings;
 use usbd_hid::hid_class::{HIDClass, HidProtocol, HidSubClass};
-use usbd_hid::{descriptor::generator_prelude::*, hid_class::HidClassSettings};
 
-#[gen_hid_descriptor(
-    (collection = APPLICATION, usage_page = GENERIC_DESKTOP, usage = 0x05) = {
-        // Button packet
-        (collection = PHYSICAL, report_id = 0x20) = {
-            (usage_page = GENERIC_DESKTOP, usage = 0x00) = {
-                #[item_settings constant,variable,absolute] unknown=input;
-            };
-            (usage_page = GENERIC_DESKTOP, usage = 0x3b) = {
-                #[item_settings data,variable,absolute] payload_size=input;
-            };
-            (usage_page = BUTTON, usage_min = 1, usage_max = 16) = {
-                #[packed_bits 16] #[item_settings data,variable,absolute] buttons=input;
-            };
-            (usage_page = GENERIC_DESKTOP,) = {
-                (usage = 0x33,) = {
-                    #[item_settings data,variable,absolute] rx=input;
-                };
-                (usage = 0x34,) = {
-                    #[item_settings data,variable,absolute] ry=input;
-                };
-            };
-            (usage_page = GENERIC_DESKTOP, usage = POINTER,) = {
-                (collection = PHYSICAL,) = {
-                    (usage_page = GENERIC_DESKTOP,) = {
-                        (usage = X,) = {
-                            #[item_settings data, variable, absolute] x=input;
-                        };
-                        (usage = Y,) = {
-                            #[item_settings data, variable, absolute] y=input;
-                        };
-                    };
-                };
-            };
-            (usage_page = GENERIC_DESKTOP, usage = POINTER,) = {
-                (collection = PHYSICAL,) = {
-                    (usage_page = GENERIC_DESKTOP,) = {
-                        (usage = Z,) = {
-                            #[item_settings data, variable, absolute] z=input;
-                        };
-                        (usage = 0x35,) = {
-                            #[item_settings data, variable, absolute] rz=input;
-                        };
-                    };
-                };
-            };
-        };
-        // Xbox button packet
-        (collection = PHYSICAL, report_id = 0x07) = {
-            (usage_page = GENERIC_DESKTOP,usage = 0x00) = {
-                #[item_settings constant, variable, absolute] unknown2=input;
-            };
-
-            (usage_page = GENERIC_DESKTOP, usage = 0x3b) = {
-                #[item_settings constant, variable, absolute] payload_size2=input;
-            };
-
-            (usage_page = BUTTON, usage = 0x10) = {
-                #[packed_bits 1] #[item_settings data, variable, absolute] xbox_button=input;
-            }
-        };
-    }
-)]
-struct GamepadReport {
-    // Normal buttons
-    unknown: [u8; 2],
-    payload_size: u8,
-    buttons: u16,
-    rx: u16,
-    ry: u16,
-    x: i16,
-    y: i16,
-    z: i16,
-    rz: i16,
-    // Xbox button
-    unknown2: [u8; 2],
-    payload_size2: u8,
-    xbox_button: u8,
-}
+// Stolen from the windows xbox controller driver I think?
+const REPORT_DESCRIPTOR: [u8; 120] = [
+    0x05, 0x01, 0x09, 0x05, 0xA1, 0x01, 0xA1, 0x00, 0x09, 0x30, 0x09, 0x31, 0x15, 0x00, 0x26, 0xFF,
+    0xFF, 0x35, 0x00, 0x46, 0xFF, 0xFF, 0x95, 0x02, 0x75, 0x10, 0x81, 0x02, 0xC0, 0xA1, 0x00, 0x09,
+    0x33, 0x09, 0x34, 0x15, 0x00, 0x26, 0xFF, 0xFF, 0x35, 0x00, 0x46, 0xFF, 0xFF, 0x95, 0x02, 0x75,
+    0x10, 0x81, 0x02, 0xC0, 0xA1, 0x00, 0x09, 0x32, 0x15, 0x00, 0x26, 0xFF, 0xFF, 0x35, 0x00, 0x46,
+    0xFF, 0xFF, 0x95, 0x01, 0x75, 0x10, 0x81, 0x02, 0xC0, 0x05, 0x09, 0x19, 0x01, 0x29, 0x0A, 0x95,
+    0x0A, 0x75, 0x01, 0x81, 0x02, 0x05, 0x01, 0x09, 0x39, 0x15, 0x01, 0x25, 0x08, 0x35, 0x00, 0x46,
+    0x3B, 0x10, 0x66, 0x0E, 0x00, 0x75, 0x04, 0x95, 0x01, 0x81, 0x42, 0x75, 0x02, 0x95, 0x01, 0x81,
+    0x03, 0x75, 0x08, 0x95, 0x02, 0x81, 0x03, 0xC0,
+];
 
 #[repr(C, packed)]
-struct GamepadReportButtons {
+struct GamepadReport {
     report_id: u8,
-    // Normal buttons
-    unknown: [u8; 2],
-    payload_size: u8,
-    buttons: u16,
-    rx: u16,
-    ry: u16,
-    x: i16,  // -0x7fff is left
-    y: i16,  // -0x7fff is up
-    z: i16,  // -0x7fff is LeftTrigger2
-    rz: i16, // -0x7fff is RightTrigger2
+    packet_size: u8,
+    misc_buttons: u8,
+    buttons: u8,
+    left_trigger: u8,
+    right_trigger: u8,
+    x: i16, // -0x7fff is left
+    y: i16, // -0x7fff is up
+
+    right_x: i16,
+    right_y: i16,
+
+    unused: [u8; 6],
 }
+
+fn create_hid_class<B: UsbBus>(bus_ref: &UsbBusAllocator<B>) -> HIDClass<'_, B> {
+    HIDClass::new_with_settings(
+        bus_ref,
+        &REPORT_DESCRIPTOR,
+        60,
+        HidClassSettings {
+            device_class: 0xff,
+            protocol: HidProtocol::Keyboard,
+            subclass: HidSubClass::Xbox360,
+            ..Default::default()
+        },
+    )
+}
+
+type ControllerMapping = [hal::gpio::Pin<
+    hal::gpio::DynPinId,
+    bsp::hal::gpio::FunctionSio<SioInput>,
+    bsp::hal::gpio::PullUp,
+>; 13];
 
 #[entry]
 fn main() -> ! {
-    // info!("Program start");
-    // println!("Chom??");
     let mut pac = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
     let sio = Sio::new(pac.SIO);
 
@@ -149,8 +102,6 @@ fn main() -> ! {
     .ok()
     .unwrap();
 
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
-
     let pins = bsp::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
@@ -158,71 +109,45 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    // Need to perform clock init before using UART or it will freeze.
-    let uart = bsp::hal::uart::UartPeripheral::new(
-        pac.UART0,
-        (pins.tx.into_function(), pins.rx.into_function()),
-        &mut pac.RESETS,
-    )
-    .enable(
-        bsp::hal::uart::UartConfig::default(),
-        clocks.peripheral_clock.freq(),
-    )
-    .unwrap();
+    // let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
-    static SERIAL: StaticCell<
-        bsp::hal::uart::UartPeripheral<
-            bsp::hal::uart::Enabled,
-            pac::UART0,
-            (
-                bsp::hal::gpio::Pin<
-                    bsp::hal::gpio::bank0::Gpio0,
-                    bsp::hal::gpio::FunctionUart,
-                    bsp::hal::gpio::PullDown,
-                >,
-                bsp::hal::gpio::Pin<
-                    bsp::hal::gpio::bank0::Gpio1,
-                    bsp::hal::gpio::FunctionUart,
-                    bsp::hal::gpio::PullDown,
-                >,
-            ),
-        >,
-    > = StaticCell::new();
-
-    defmt_serial::defmt_serial(SERIAL.init(uart));
-
-    static INPUTS: StaticCell<
-        [hal::gpio::Pin<
-            hal::gpio::DynPinId,
-            bsp::hal::gpio::FunctionSio<SioInput>,
-            bsp::hal::gpio::PullUp,
-        >; 13],
-    > = StaticCell::new();
+    static INPUTS: StaticCell<[ControllerMapping; 2]> = StaticCell::new();
     let inputs = INPUTS.init_with(|| {
         [
-            pins.d11.into_pull_up_input().into_dyn_pin(), // B1
-            pins.a0.into_pull_up_input().into_dyn_pin(),  // B2
-            pins.a1.into_pull_up_input().into_dyn_pin(),  // A1
-            pins.a2.into_pull_up_input().into_dyn_pin(),  // A2
-            pins.a3.into_pull_up_input().into_dyn_pin(),  // A4
-            pins.d9.into_pull_up_input().into_dyn_pin(),  // A3
-            pins.d2.into_pull_up_input().into_dyn_pin(),  // Nothing
-            pins.d3.into_pull_up_input().into_dyn_pin(),  // South
-            pins.d4.into_pull_up_input().into_dyn_pin(),  // West
-            pins.d5.into_pull_up_input().into_dyn_pin(),  // East
-            pins.d6.into_pull_up_input().into_dyn_pin(),  // North
-            pins.d7.into_pull_up_input().into_dyn_pin(),  // B3
-            pins.d8.into_pull_up_input().into_dyn_pin(),  // B4
+            [
+                // Player one
+                pins.gpio0.into_pull_up_input().into_dyn_pin(), // A4
+                pins.gpio1.into_pull_up_input().into_dyn_pin(), // A3
+                pins.gpio2.into_pull_up_input().into_dyn_pin(), // B1
+                pins.gpio3.into_pull_up_input().into_dyn_pin(), // B2
+                pins.gpio4.into_pull_up_input().into_dyn_pin(), // A1
+                pins.gpio5.into_pull_up_input().into_dyn_pin(), // A2
+                pins.gpio6.into_pull_up_input().into_dyn_pin(), // Start button
+                pins.gpio7.into_pull_up_input().into_dyn_pin(), // North
+                pins.gpio8.into_pull_up_input().into_dyn_pin(), // West
+                pins.gpio9.into_pull_up_input().into_dyn_pin(), // East
+                pins.gpio10.into_pull_up_input().into_dyn_pin(), // South
+                pins.gpio11.into_pull_up_input().into_dyn_pin(), // B3
+                pins.gpio12.into_pull_up_input().into_dyn_pin(), // B4
+            ],
+            [
+                // Player two
+                pins.gpio13.into_pull_up_input().into_dyn_pin(),
+                pins.gpio14.into_pull_up_input().into_dyn_pin(),
+                pins.gpio15.into_pull_up_input().into_dyn_pin(),
+                pins.gpio16.into_pull_up_input().into_dyn_pin(),
+                pins.gpio17.into_pull_up_input().into_dyn_pin(),
+                pins.gpio18.into_pull_up_input().into_dyn_pin(),
+                pins.gpio19.into_pull_up_input().into_dyn_pin(),
+                pins.gpio20.into_pull_up_input().into_dyn_pin(),
+                pins.gpio21.into_pull_up_input().into_dyn_pin(),
+                pins.gpio22.into_pull_up_input().into_dyn_pin(),
+                pins.gpio26.into_pull_up_input().into_dyn_pin(),
+                pins.gpio27.into_pull_up_input().into_dyn_pin(),
+                pins.gpio28.into_pull_up_input().into_dyn_pin(),
+            ],
         ]
     });
-
-    // loop {
-    //     delay.delay_ms(1000);
-    //     defmt::info!("Chom");
-    // }
-
-    info!("CHom chom skz");
-    println!("Chom??");
 
     // Create the USB Bus Allocator
     let usb_bus = UsbBusAllocator::new(hal::usb::UsbBus::new(
@@ -239,26 +164,8 @@ fn main() -> ! {
     };
 
     // Set up the USB HID Class Device driver, providing Mouse Reports
-    let player_one = HIDClass::new_with_settings(
-        bus_ref,
-        GamepadReport::desc(),
-        60,
-        HidClassSettings {
-            device_class: 0xff,
-            ..Default::default()
-        },
-    );
-    let player_two = HIDClass::new_with_settings(
-        bus_ref,
-        GamepadReport::desc(),
-        60,
-        HidClassSettings {
-            device_class: 0xff,
-            protocol: HidProtocol::Keyboard,
-            subclass: HidSubClass::Xbox360,
-            ..Default::default()
-        },
-    );
+    let player_one = create_hid_class(bus_ref);
+    let player_two = create_hid_class(bus_ref);
 
     // Create a USB device with a fake VID and PID
     let usb_device = UsbDeviceBuilder::new(bus_ref, UsbVidPid(0x24c6, 0xfafe))
@@ -272,72 +179,71 @@ fn main() -> ! {
 
     critical_section::with(|token| {
         *GADGET.borrow_ref_mut(token) = Some(Gadget {
-            player_one,
-            player_two,
+            players: [player_one, player_two],
             usb_device,
         });
     });
 
-    unsafe {
-        // Enable the USB interrupt
-        pac::NVIC::unmask(hal::pac::Interrupt::USBCTRL_IRQ);
-    };
-
     // Move the cursor up and down every 200ms
-    let mut sequence = 0;
-    let mut buttons = GamepadReportButtons {
-        report_id: 0x20, // Button packet
-        unknown: [0, sequence],
-        payload_size: 0x0e, //core::mem::size_of::<GamepadReportButtons>() as u8,
+    let mut report = GamepadReport {
+        report_id: 0x00, // Button packet
+        packet_size: core::mem::size_of::<GamepadReport>() as u8,
+        misc_buttons: 0,
         buttons: 0,
-        rx: 0,
-        ry: 0,
+        left_trigger: 0,
+        right_trigger: 0,
         x: 0,
         y: 0,
-        z: 0,
-        rz: 0,
+        right_x: 0,
+        right_y: 0,
+        unused: [0, 0, 0, 0, 0, 0],
     };
 
     loop {
-        buttons.buttons = 0;
-        buttons.x = 0;
-        buttons.y = 0;
-        buttons.rz = i16::MIN;
-        buttons.z = i16::MIN;
-        for (index, input) in inputs.iter_mut().enumerate() {
-            let state = input.is_high().unwrap();
-            if state {
-                continue;
+        for (player_index, inputs) in inputs.iter_mut().enumerate() {
+            report.misc_buttons = 0;
+            report.buttons = 0;
+            report.left_trigger = 0;
+            report.right_trigger = 0;
+            report.x = 0;
+            report.y = 0;
+            for (index, input) in inputs.iter_mut().enumerate() {
+                let state = input.is_high().unwrap();
+                if state {
+                    continue;
+                }
+                if index < 2 {
+                    // Actual buttons
+                    report.buttons = 1 << index;
+                } else if index < 2 + 4 {
+                    report.buttons = 1 << (index + 2);
+                } else if index < 2 + 4 + 1 {
+                    report.misc_buttons = 1 << 4;
+                } else if index < 2 + 4 + 1 + 4 {
+                    // Joysticks
+                    let value = if index & 1 == 1 { i16::MAX } else { i16::MIN };
+                    if index & 2 == 0 {
+                        report.x = value;
+                    } else {
+                        report.y = value;
+                    }
+                } else if index < 2 + 4 + 1 + 4 + 2 {
+                    // Triggers
+                    if index & 1 == 0 {
+                        report.left_trigger = u8::MAX;
+                    } else {
+                        report.right_trigger = u8::MAX;
+                    }
+                }
             }
-            if index < 7 {
-                // Actual buttons
-                buttons.buttons = 1 << index;
-            } else if index < 7 + 4 {
-                // Joysticks
-                let value = if index & 1 == 1 { i16::MAX } else { i16::MIN };
-                if index & 2 == 0 {
-                    buttons.x = value;
-                } else {
-                    buttons.y = value;
-                }
-            } else if index < 7 + 4 + 2 {
-                // Triggers
-                if index & 1 == 0 {
-                    buttons.z = i16::MAX;
-                } else {
-                    buttons.rz = i16::MAX;
-                }
+            if let Err(err) = push_movement(&report, player_index) {
+                info!(
+                    "Error pushing movement to controller {:?}",
+                    defmt::Debug2Format(&err)
+                );
             }
         }
-        // info!("Running a tick of the wiggler");
-        buttons.unknown[1] = sequence;
-        sequence = sequence.wrapping_add(1);
-        let result = push_movement(&buttons);
-        if let Err(err) = result {
-            info!("Got an error from pushing: {:?}", defmt::Debug2Format(&err));
-        }
-        // delay.delay_ms(1);
-        // info!("Result gave {:?}", defmt::Debug2Format(&result));
+        poll_usb();
     }
 }
 
@@ -348,44 +254,41 @@ unsafe fn any_as_u8_slice<T: Sized>(p: &T) -> &[u8] {
 /// Submit a new mouse movement report to the USB stack.
 ///
 /// We do this with interrupts disabled, to avoid a race hazard with the USB IRQ.
-fn push_movement(report: &GamepadReportButtons) -> Result<usize, usb_device::UsbError> {
+fn push_movement(
+    report: &GamepadReport,
+    player_index: usize,
+) -> Result<usize, usb_device::UsbError> {
+    info!("Pushing a movement");
     critical_section::with(|token| {
+        info!("Pushing a movement acquired lock");
         // Now interrupts are disabled, grab the global variable and, if
         // available, send it a HID report
         let input = unsafe { any_as_u8_slice(report) };
         info!("Input was: {}", input.len());
-        GADGET.borrow_ref_mut(token).as_ref().map(|gadget| {
-            gadget
-                .player_one
-                .push_raw_input(input)
-                .and_then(|b| gadget.player_two.push_raw_input(input).map(|a| a + b))
-        })
+        GADGET
+            .borrow_ref_mut(token)
+            .as_ref()
+            .map(|gadget| gadget.players[player_index].push_raw_input(input))
     })
     .unwrap()
 }
 
 struct Gadget {
     usb_device: UsbDevice<'static, hal::usb::UsbBus>,
-    player_one: HIDClass<'static, hal::usb::UsbBus>,
-    player_two: HIDClass<'static, hal::usb::UsbBus>,
+    players: [HIDClass<'static, hal::usb::UsbBus>; 2],
 }
 
 /// The USB Device Driver (shared with the interrupt).
 static GADGET: Mutex<RefCell<Option<Gadget>>> = Mutex::new(RefCell::new(None));
 
-/// This function is called whenever the USB Hardware generates an Interrupt
-/// Request.
-#[allow(non_snake_case)]
-#[interrupt]
-fn USBCTRL_IRQ() {
+fn poll_usb() {
     // Handle USB request
     critical_section::with(|token| {
         GADGET.borrow_ref_mut(token).as_mut().map(|gadget| {
+            let mut players = gadget.players.iter_mut();
             gadget
                 .usb_device
-                .poll(&mut [&mut gadget.player_one, &mut gadget.player_two])
+                .poll(&mut [players.next().unwrap(), players.next().unwrap()]);
         })
     });
 }
-
-// End of file
